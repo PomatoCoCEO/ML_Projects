e6e5b9c8d04a313bc3577b58ed0804b5e97a9a14
PomatoCoCEO <paulocortesao@student.dei.uc.pt>
2022-10-15 21:42:05 +0100

    DEV: minor changes on classifiers

--- code/classifier_one_layer.m
+++ code/classifier_one_layer.m
@@ -6,11 +6,13 @@ function net = classifier_one_layer(P,T, actFuncStr, addSoftMax)
     net.name="One Layer Classifier "+actFuncStr;
     st = size(T);
     net.adaptFcn = "learngd"; % gradient method by default
+    net.trainFcn = "trainr";
     if actFuncStr == "hardlim"
         if ~addSoftMax
             net.adaptFcn="learnp"; % perceptron weight and bias learning function
             net.trainFcn = "trainc";
         else
+            net.name="One Layer Classifier "+actFuncStr+" with Softmax";
             net.layers{1}.transferFcn = "softmax";
         end
     elseif addSoftMax
@@ -25,7 +27,7 @@ function net = classifier_one_layer(P,T, actFuncStr, addSoftMax)
     end
     net = init(net);
     net = configure(net, P,T);
-    net.trainParam.epochs = 10;
+    net.trainParam.epochs = 100;
     net.trainParam.goal = 1e-6;
     net.trainParam.lr = 0.1; 
     view(net);
