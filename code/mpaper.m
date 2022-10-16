aefde38e193d92a95fc837c900c849efd1377900
Andre Figo <nefigo2001@gmail.com>
2022-10-16 17:53:33 +0100

    Interface tested and working

--- code/mpaper.m
+++ code/mpaper.m
@@ -206,7 +206,7 @@ function Dn(varargin)
  % We want to save only P, and for that we must specify that we only want
  % to save P; for that we must write
  
-              save P.mat P
+              save ../data/P.mat P
               Choose_classifier();
  % and a mat file called P is created having inside the matrix P.Then we can
  % load P, and rename the matrix P, by clicking on P with the right mouse
@@ -218,7 +218,7 @@ function Dn(varargin)
  
  % By the same reason, to save only ind we write
  
-             save ind.mat ind
+             save ../data/ind.mat ind
              
  % The P.mat and in.mat are saved in the actual working directory of Matlab.
              
--- code/my_classify.m
+++ code/my_classify.m
@@ -1,16 +1,80 @@
 function C = my_classify(classifier_name, act_func, with_softmax)
     disp(act_func)
     disp(with_softmax)
+    folderName = "nets_100ep_1000_inputs";
+    load("../data/P.mat");
+    test_input=P;
+    load("../data/ind.mat");
+    load("../data/PerfectArial.mat");
+
     if classifier_name == "AssMem"
-        disp(1)
+        load("../data/"+folderName+"/AFW.mat"); % associative layer
+        net_name = "1layer_c_";
+        net_name = net_name + act_func;
+        if with_softmax
+            net_name = net_name+ "_softmax.mat";
+        else
+            net_name = net_name+ "_no_softmax.mat";
+        end
+        load("../data/"+ folderName+"/"+net_name);
+        output_assoc = weights * test_input;
+        out_sim = sim(net, output_assoc);
 
     elseif classifier_name == "Percept"
-         disp(2)
+        load("../data/"+folderName+"/perceptron.mat"); 
+        perceptron = net;
+        net_name = "perceptron1C_";
+        net_name = net_name + act_func;
+        if with_softmax
+            net_name = net_name+ "_softmax.mat";
+        else
+            net_name = net_name+ "_no_softmax.mat";
+        end
+        load("../data/"+ folderName+"/"+net_name);
+        output_bp = sim(perceptron, test_input);
+        out_sim = sim(net, output_bp);
     elseif classifier_name == "OneLayer"
-        disp(3)
+        net_name = "1layer_";
+        net_name = net_name + act_func;
+        if with_softmax
+            net_name = net_name+ "_softmax.mat";
+        else
+            net_name = net_name+ "_no_softmax.mat";
+        end
+        load("../data/"+ folderName+"/"+net_name);
+        out_sim = sim(net, test_input);
+        
+        
+
     elseif classifier_name == "TwoLayer"
-        disp(4)
+        net_name = "2layer_";
+        net_name = net_name + act_func;
+        if with_softmax
+            net_name = net_name+ "_softmax.mat";
+        else
+            net_name = net_name+ "_no_softmax.mat";
+        end
+        load("../data/"+ folderName+"/"+net_name);
+        out_sim = sim(net, test_input);
+        
     elseif classifier_name == "Pattern"
-        disp(5)
+        net_name = "pattern.mat";
+        load("../data/"+ folderName+"/"+net_name);
+        out_sim = sim(net, test_input);
     end
+
+
+    
+
+    conv_out =  convert_output(out_sim);
+    digits_out = one_hot_to_digit(conv_out);
+    
+    predicted = zeros(size(P));
+    sp = size(P);
+    for i= 1:sp(2)
+        predicted(:,i)= Perfect(:,digits_out(i));
+    end
+    predicted=predicted(:,ind);
+    figure;
+    showim(predicted);
 end
