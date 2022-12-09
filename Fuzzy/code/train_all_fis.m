61be550615d98bb2caa421df035138ab4417ec8e
PomatoCoCEO <paulomiguelc@gmail.com>
2022-12-09 10:15:19 +0000

    DEV: new controllers and registering function

--- Fuzzy/code/train_all_fis.m
+++ Fuzzy/code/train_all_fis.m
@@ -1,10 +1,13 @@
 function errors = train_all_fis(training_data, test_data)
     opt_methods = [0,1];
+    op_method_names = ["Backpropagation", "Hybrid"];
     clustering_methods = ["GridPartition", "SubtractiveClustering", "FCMClustering"];
     train_input = training_data(:, 1:end-1);
     train_output = training_data(:, end);
-    test_input = training_data(:, 1:end-1);
-    test_output = training_data(:, end);
+    test_input = test_data(:, 1:end-1);
+    test_output = test_data(:, end);
+    str_file = "fis/fis_results.txt";
+    fileSseID = fopen(str_file, "a");
     for i = 1:3
         clustering_method = clustering_methods(i);
         genOptions = genfisOptions(clustering_method);
@@ -15,8 +18,9 @@ function errors = train_all_fis(training_data, test_data)
             str_file = "fis/"+clustering_method+"_"+j;
             writeFIS(trained_fis, str_file);
             result = evalfis(trained_fis,test_input);
-            mse = sum((result - test_output).^2)/numel(result);
-            fprintf("Clustering method: %s; opt method: %d, mse: %d", clustering_method, j, mse);
+            sse = sum((result - test_output).^2);% /numel(result);
+            no_rules = size(trained_fis.rules, 2);
+            fprintf(fileSseID,"Clustering method: %s; opt method: %s, no. of rules:%d, sse: %g\n", clustering_method, op_method_names(j+1), no_rules, sse);
         end
     end
 end
