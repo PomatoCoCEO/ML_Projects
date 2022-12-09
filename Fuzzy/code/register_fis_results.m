e1aa47408ce5d8e038cab38f7289b7ad659e434b
PomatoCoCEO <paulomiguelc@gmail.com>
2022-12-09 10:37:31 +0000

    DEV: a few updates

--- Fuzzy/code/register_fis_results.m
+++ Fuzzy/code/register_fis_results.m
@@ -2,6 +2,6 @@ function register_fis_results(clustering_method, no_rules, type_function, opt_me
     % register: no of rules (3, 5 or 7)
     % scale factor 1: for output magnification
     % scale factor 2: for actuator magnification
-    fileID = fopen('register_2.csv','a');
+    fileID = fopen('fis/results.csv','a');
     fprintf(fileID,'%s,%d,%s,%s,%g\n',clustering_method,no_rules, type_function,opt_method,performance);
 end
